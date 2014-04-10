993ca1d3a5ee7880ba1f8871e7aefafa2405fc2a
Deshan Dissanayake <deshan.nd13@yahoo.com>
2014-04-10 13:58:31 +0800

    Initial Rails project created

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+EnrolPlanner::Application.config.secret_key_base = 'e75abcb34da908216df86046286d35a00f1869461fb0150de8f73fed3f4f15b85637fb0e3d5f6157eb332455ade0530c4fcd09efe1f620ab5f288d51473c840f'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+EnrolPlanner::Application.config.session_store :cookie_store, key: '_enrol_planner_session'
