353711f390f435cb9777807684a551e1f66dce3e
Spike Mak <msh.spike@gmail.com>
2014-05-20 05:18:39 +0800

    Define session variables, add dynamic render sembox and done units on enrolment_planner, cleanup planner controller

--- app/controllers/planner_controller.rb
+++ app/controllers/planner_controller.rb
@@ -1,25 +1,35 @@
 class PlannerController < ApplicationController
-	#@selected_stream
 	@stream_units
 
-	before_filter :get_selected_stream
-
-	helper_method :get_streamunit_name
+	helper_method :get_streamunit_name, :get_stream_name
 
 # START stream_chooser
 	def index
 		@streams = Stream.all
+
+		session.delete(:select_stream)
+		session.delete(:semesters)
+		session.delete(:done_units)
+		session.delete(:remain_units)
 	end
+
+	def get_stream_name sid
+		s = Stream.find_by_id(sid)
+		return s.streamName
+	end
+
 # END stream_chooser
 
 
 # START unit_chooser
 	def unit_chooser
-		@selected_stream = Stream.find_by_id(params["streamSelect"])
+		@str = Stream.find_by_id(params["streamSelect"])
 
 		#declare session variable and store selected stream id to it
-		(session[:select_stream] ||= []) && (session[:select_stream] = @selected_stream.id )
-		@stream_units = getStreamUnits(@selected_stream)
+		session[:select_stream] = @str.id
+
+		
+		@stream_units = getStreamUnits(@str)
 	end
 
 	def get_streamunit_name uid
@@ -38,22 +48,36 @@ class PlannerController < ApplicationController
 # START enrolment_planner
 	def enrolment_planner
 		@done_units = []
+		session[:done_units] = params[:unit_ids]
+		session[:remain_units] = []
+
+		# START initialising session[:semesters]
+			session[:semesters]=[]
+			session[:semesters][0] = []
+			session[:semesters][0][0] = 0
+		# END initialising
+		session[:semesters][0] = params[:unit_ids] # For testing purpose only
+		session[:semesters][1] = params[:unit_ids] # For testing purpose only
 
 		params[:unit_ids].each do |puid|
 			@done_units += StreamUnit.where(:stream_id => session[:select_stream]).where(:unit_id => (puid.to_i))
 		end
 		
 		@remain_units = getRemainingUnits(@done_units)
+		@remain_units.each do |ru|
+			session[:remain_units] << ru.id
+		end
 	end
 
 	def getRemainingUnits(done)			
 		remain_streamunits = StreamUnit.where(:stream_id => session[:select_stream]).where('id not in (?)', done)
 		return remain_streamunits
 	end
-# END enrolment_planner
-	def get_selected_stream
-		return @selected
+
+	def get_semester_done_units sem
+		
 	end
+# END enrolment_planner
 end
 
 # START AJAX Testing
