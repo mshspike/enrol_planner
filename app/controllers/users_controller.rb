6b683a4fcde056dade289ab6ea6a9d22785c5cd5
Pauline <pohling928@gmail.com>
2014-06-05 19:26:06 +0800

    minor changes

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,7 @@
 class UsersController < ApplicationController
   before_action :set_user, only: [:show, :edit, :update, :destroy]
-  before_filter :require_login, except: [:new]
+  before_filter :require_login, except: [:new, :create]
+
 
 
   # GET /users
