93839a46512f3d22dd24347afb850b667d8a1183
Spike Mak <msh.spike@gmail.com>
2014-08-26 11:04:33 +0800

    remove old 'group column' data from seed file

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140823130616) do
+ActiveRecord::Schema.define(version: 20140826025554) do
 
   create_table "pre_req_groups", force: true do |t|
     t.integer  "unit_id"
